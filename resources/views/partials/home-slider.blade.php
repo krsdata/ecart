d31c9e2137e2069854850e27110cff12cb30e152
kandy <kroy.iips@gmail.com>
2017-04-10 17:30:45 +0000

    css update

--- resources/views/partials/home-slider.blade.php
+++ resources/views/partials/home-slider.blade.php
@@ -3,12 +3,12 @@
           <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
             <div class="item" style="background-image: url({{ asset('public/enduser/assets/images/sliders/01.jpg') }});">
               <div class="container-fluid">
-                <div class="caption bg-color vertical-center text-left">
+                <!--<div class="caption bg-color vertical-center text-left">
                   <div class="slider-header fadeInDown-1">Top Brands</div>
                   <div class="big-text fadeInDown-1"> New Collections </div>
                   <div class="excerpt fadeInDown-2 hidden-xs"> <span></span> </div>
                   <div class="button-holder fadeInDown-3"> <a href="index.php-page=single-product.htm" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a> </div>
-                </div>
+                </div>-->
                 <!-- /.caption --> 
               </div>
               <!-- /.container-fluid --> 
@@ -18,8 +18,9 @@
             <div class="item" style="background-image: url({{ asset('public/enduser/assets/images/sliders/02.jpg')}});">
               <div class="container-fluid">
                 <div class="caption bg-color vertical-center text-left">
-                  <div class="slider-header fadeInDown-1">Spring 2017</div>
-                  <div class="big-text fadeInDown-1">Women Fashion </div>
+                  <div class="slider-header fadeInDown-1">Harbal</div>
+                  <div class="big-text fadeInDown-1">Muscels Growth </div>
+                  <p class="slide-para">Muscles Growth Powder is a 100% Ayurvedic Body Growth System. Body Growth offers Amazing Results! After using Body Growth Height Ensures Powder you will see new confidence in yourself.</p>
                   <div class="excerpt fadeInDown-2 hidden-xs"> <span> </span> </div>
                   <div class="button-holder fadeInDown-3"> <a href="index.php-page=single-product.htm" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a> </div>
                 </div>
