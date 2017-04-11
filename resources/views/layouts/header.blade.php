e04d6dc18a6a9d174c82057a5a85a390249a982a
krsdata <krsdata.net@gmail.com>
2017-04-12 02:21:29 +0530

    loader

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -41,8 +41,34 @@
 	z-index: 9999;
 	background: url(images/loader-64x/Preloader_2.gif) center no-repeat #fff;
 }
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+.se-pre-con.loader {
+    margin: 0 auto; 
+    left: 44.5%;
+    top: 44.5%;
+}
+
 </style>
 </head>
 <body class="cnt-home">
-<div class="se-pre-con">loading...</div> 
+<div class="se-pre-con loader"></div> 
  
