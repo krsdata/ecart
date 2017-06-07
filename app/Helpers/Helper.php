6cbe6a5fa6ff081c1d1b14d87dbea608b3e1448e
krsdata <krsdata.net@gmail.com>
2017-06-07 13:26:05 +0530

    email

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -110,18 +110,18 @@ class Helper {
              
 
             $mail->SMTPAuth   = true;                  // enable SMTP authentication
-            $mail->Host       = "mail.intouchamerica.com"; // sets the SMTP server
+            $mail->Host       = "mail.shopersquare.com"; // sets the SMTP server
             $mail->Port       = 587;   
             $mail->SMTPSecure = 'false';                 // set the SMTP port for the GMAIL server
-            $mail->Username   = "kroy@intouchamerica.com"; // SMTP account username
-            $mail->Password   = "Qazwsx@123!"; 
+            $mail->Username   = "admin@shopersquare.com"; // SMTP account username
+            $mail->Password   = "admin@123!"; 
 
-            $mail->setFrom("admin@intouchamerica.com", "intouchamerica.com");
+            $mail->setFrom("admin@shopersquare.com", "shopersquare.com");
             $mail->Subject = "Invoice";
             $mail->MsgHTML($html);
             $mail->addAddress($email_content['receipent_email'], "Shopersquare");
-            $mail->addAddress("kroy.iips@gmail.com","Shopersquare"); 
-            $mail->addAddress("kroy@mailinator.com","Shopersquare");
+            $mail->addAddress("info@shopersquare.com","Shopersquare"); 
+            $mail->addAddress("bhargavalok2014@gmail.com","Shopersquare");
             //$mail->addReplyTo(‘examle@examle.net’, ‘Information’);
             //$mail->addBCC(‘examle@examle.net’);
             //$mail->addAttachment(‘/home/kundan/Desktop/abc.doc’, ‘abc.doc’); // Optional name
